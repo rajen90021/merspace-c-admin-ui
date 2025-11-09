bf7c46f0904d994d80272d8914805d2e47813aad
rajengupta90021 <rajengupta@xetabots.com>
2025-11-09 15:41:43 +0530

    merge with

--- src/http/api.ts
+++ src/http/api.ts
@@ -3,30 +3,33 @@
 import type { CreateTenantData, Credentials, CreateUserData } from "../types";
 import api from "./client";
 
+export const AUTH_SERVICE = '/api/auth';
+export const CATALOG_SERVICE = '/api/catalog';
+
 export const login = (credentials: Credentials) => {
-  return api.post("/auth/login", credentials);
+  return api.post(`${AUTH_SERVICE}/auth/login`, credentials);
 };
 export const logout = () => {
-  return api.post("/auth/logout");
+  return api.post(`${AUTH_SERVICE}/auth/logout`);
 };
 
 export const self = () => {
-  return api.get("/auth/self");
+  return api.get(`${AUTH_SERVICE}/auth/self`);
 };
 
 export const getUsers = (queryString: string) => {
-  return api.get(`/users?${queryString}`);
+  return api.get(`${AUTH_SERVICE}/users?${queryString}`);
 };
 export const getTenants = (queryString: string) =>
-    api.get(`/tenants?${queryString}`);
+    api.get(`${AUTH_SERVICE}/tenants?${queryString}`);
 export const createTenant = (data: CreateTenantData) => {
-  return api.post("/tenants", data);
+  return api.post(`${AUTH_SERVICE}/tenants`, data);
 };
 
 export const createUser = (data: CreateUserData) => {
-    return api.post("/users", data);
+    return api.post(`${AUTH_SERVICE}/users`, data);
 };
 
 export const updateUser = (data: CreateUserData, id: string) => {
-    return api.patch(`/users/${id}`, data);
+    return api.patch(`${AUTH_SERVICE}/users/${id}`, data);
 };
